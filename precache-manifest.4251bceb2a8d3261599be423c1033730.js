fe5d833a471cfe716351d25d50328f7b17b3b91f
winsbu1997 <winsbu1997@gmail.com>
2020-06-18 23:31:55 +0700

    Updates

---
+++ precache-manifest.4251bceb2a8d3261599be423c1033730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ae5dc5c49d12a4c495f2090ffdfb67",
+    "url": "/Mta_React_Web/index.html"
+  },
+  {
+    "revision": "308d42a681f227b3ab6b",
+    "url": "/Mta_React_Web/static/css/2.48e9bf46.chunk.css"
+  },
+  {
+    "revision": "807713864e5eda18b1dc",
+    "url": "/Mta_React_Web/static/css/main.e7ca69c0.chunk.css"
+  },
+  {
+    "revision": "308d42a681f227b3ab6b",
+    "url": "/Mta_React_Web/static/js/2.e572c24d.chunk.js"
+  },
+  {
+    "revision": "1f343f3b00bd42841cd23cdb6e2b08ec",
+    "url": "/Mta_React_Web/static/js/2.e572c24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807713864e5eda18b1dc",
+    "url": "/Mta_React_Web/static/js/main.d7d6b637.chunk.js"
+  },
+  {
+    "revision": "b910daddf04e49b871c5",
+    "url": "/Mta_React_Web/static/js/runtime-main.8f39eccc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Mta_React_Web/precache-manifest.4251bceb2a8d3261599be423c1033730.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mta_React_Web/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
